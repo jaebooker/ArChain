97f112c351114772711568d9fe8c787798fdbcd0
jaebooker <jaesonbooker@gmail.com>
2019-07-04 16:23:36 -0700

    working on tests

--- contracts/ArCacheToken.sol
+++ contracts/ArCacheToken.sol
@@ -9,18 +9,21 @@ pragma solidity >=0.5.0 <0.6.0;
 import './node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
 import "../../math/SafeMath.sol";
 
-contract ArCacheToken is ERC721Token {
+contract ArCacheToken is ERC721Token, Ownable {
   /// @author Jaeson Booker
   /// @notice Inherists from Openzeppelin's ERC721 Token
   /// @dev could find better, more decentralized way of handling token minting
   /// @param uses SafeMath and Openzeppelin
   /// @return mints tokens that describe texture and type for mobile UI yet to be fully implemented
   using SafeMath for uint256;
+  //private erc address
   bytes4 private constant _ERC721_RECEIVED = 0x150b7a02;
 
   // Mapping from token ID to owner
   mapping (uint256 => address) private _tokenOwner;
   constructor() ERC721("ArCache Token", "ARCT") public {}
+
+  owner = msg.sender
 
   //object struct, containing texture and type (sword, coin, etc.)
   struct Object {
@@ -34,13 +37,13 @@ contract ArCacheToken is ERC721Token {
     Object memory _object = Object(uint8(now), uint8(now-1000));
     uint _id = objects.push(_object) - 1;
     //mints to special address, so people can't create and own tokens outside of mobile app
-    _mint(address(0), _id);
+    _mint(_ERC721_RECEIVED, _id);
   }
   //this function will be called by app, transferring from special address to user
   //this way people have to actually go to the location of an ArCache, rather than cheating by simply
   //calling the mint() function on the contract
   function transferFrom(address from, address to, uint256 tokenId) public {
-      require(owner == msg.sender)
+      require(owner == _ERC721_RECEIVED)
       _transferFrom(address from, address to, uint256 tokenId)
   }
 
--- test/TestArCacheToken.js
+++ test/TestArCacheToken.js
@@ -2,29 +2,27 @@ var ArCacheToken = artifacts.require("ArCacheToken");
 
 contract('ArCacheToken', function(accounts){
 
-  it("should put 1 token in the owners account", function(){
+    it("Should make first account an owner", async () => {
+        let instance = await ArCacheToken.deployed();
+        let owner = await instance.owner();
+        assert.equal(owner, accounts[0]);
+        });
+    });
+
+  it("should put 0 tokens in the owners account", function(){
     return ArCacheToken.deployed().then(function(instance){
+      instance.mint()
       return instance.balance.call(accounts[0]);
     }).then(function(balance){
-      assert.equal(balance.valueOf(), 1, "Token not retrieved");
+      assert.equal(balance.valueOf(), 0, "Something went wrong");
     });
   });
 
   it("should return the caches texture and shape", function(){
     return ArCacheToken.deployed().then(function(instance){
-      return instance.getName.call();
-    }).then(function(name){
-      assert.equal(name, "gold, sword", "shape and texture was not defined");
-    });
-  });
-
-  it("should return the texture gold", function(){
-    return ArCacheToken.deployed().then(function(instance){
-      return instance.setTexture('Gold').then(function(){
-        return instance.getName.call();
-      }).then(function(name){
-        assert.equal(name, "Gold", "the texture was not gold");
-      });
+      return instance.getObjectVisualFromId(uint id).call();
+  }).then(function(instance){
+      assert.equal(instance, "gold, sword", "shape and texture was not defined");
     });
   });
 });
